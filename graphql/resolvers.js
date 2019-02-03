4f65c05fd20ba418249428c81fbf9f4ade0e94bc
minung.han <hmu332233@gmail.com>
2019-02-03 22:59:58 +0900

    [Add] 좀더 복잡한 쿼리 연습

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,7 +1,32 @@
+const people = [
+  {
+    name: 'minung',
+    age: 27,
+    gender: 'female'
+  },
+  {
+    name: 'minung2',
+    age: 2,
+    gender: 'female'
+  },
+  {
+    name: 'minung3',
+    age: 3,
+    gender: 'female'
+  },
+  {
+    name: 'minung4',
+    age: 4,
+    gender: 'female'
+  }
+]
+
+
 // query를 해결해주는 것
 const resolvers = {
   Query: {
-    name: () => 'minung'
+    name: () => 'minung',
+    people: () => people
   }
 }
 
