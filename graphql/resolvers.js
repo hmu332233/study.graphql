829b990e3781414af020e2d08e0214d645493d46
minung.han <hmu332233@gmail.com>
2019-02-07 01:47:55 +0900

    [Add] 외부 api를 통한 graphql 사용

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,16 +1,28 @@
-const { addMovie, getMovies, getById, deleteMovie } = require('./db');
+// const { addMovie, getMovies, getById, deleteMovie } = require('./db');
+const { getMovies } = require('./api');
 
 // query를 해결해주는 것
+// const resolvers = {
+//   Query: {
+//     name: () => 'minung',
+//     movies: () => getMovies(),
+//     movie: (_, { id }) => getById(id)
+//   },
+//   Mutation: {
+//     addMovie: (_, { name, score }) => addMovie({ name, score }),
+//     deleteMovie: (_, { id }) => deleteMovie(id)
+//   }
+// }
 const resolvers = {
   Query: {
-    name: () => 'minung',
+    
     movies: () => getMovies(),
-    movie: (_, { id }) => getById(id)
+    
   },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie({ name, score }),
-    deleteMovie: (_, { id }) => deleteMovie(id)
-  }
+  // Mutation: {
+  //   addMovie: (_, { name, score }) => addMovie({ name, score }),
+  //   deleteMovie: (_, { id }) => deleteMovie(id)
+  // }
 }
 
 module.exports = resolvers;
